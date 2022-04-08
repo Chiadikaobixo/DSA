f94572d1353a7a641a438d97082e20f7e9ea9656
chiadikaobixo <chiadikaobixo@gmail.com>
2022-04-08 05:21:06 -0500

    multiple pointer approach

--- SolvingPatterns/README.md
+++ SolvingPatterns/README.md
@@ -1,2 +1,5 @@
-## FrequencyCounter
+## Frequency Counter
 This pattern uses objects or sets to collect values/frequencies of values which is usually o(n) time. This can avoid the need of nested loops or O(n^2) operations with arrays or strings.
+
+## Multiple Pointer
+This pattern creates pointers or value that correspond to an index or position and move towards the beginning, end, middle based on a certian condition. It's very efficient in solving problem with minimal space complexity as well. 
---
+++ SolvingPatterns/multiplePointer.js
@@ -0,0 +1,57 @@
+// write a function called sumZero with accepts a sorted array
+// of integers. The function should find the first pair where the
+// sum is 0. Return an array the includes both values that sums to
+// zero or undefined if a pair does not exist.
+
+
+// O(N^2) Time complexity
+// O(1) Space complexity
+const sumZero = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+}
+
+
+// Multiple Pointers Approach 
+// O(N) Time complexity
+// O(1) Space complexity
+const sumToZero = (arr) => {
+    let left = 0
+    let right = arr.length - 1
+    while (left < right) {
+        let sum = arr[left] + arr[right]
+        if (sum === 0) {
+            return [arr[left], arr[right]]
+        } else if (sum > 0) {
+            right--
+        } else {
+            left++
+        }
+    }
+}
+
+
+// Implement a function called countUniqueValues which accepts
+// a sorted arrays, and count the unique values in the array.
+// There can be negative numbers in the array, but it will always
+// be sorted.
+
+// Multiple Pointers Approach 
+// O(N)
+// O(1) space complexity
+const countUniqueValues = (arr) => {
+    if(arr.length === 0) return 0
+    let i = 0
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++
+            arr[i] = arr[j]
+        } 
+    }
+    return i + 1
+}
