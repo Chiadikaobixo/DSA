a2a9d346da668ca7194e3b94e9756b4b676fc19a
chiadikaobixo <chiadikaobixo@gmail.com>
2022-04-08 08:06:38 -0500

    sliding window pattern

---
+++ ProblemSolving/README.md
@@ -0,0 +1,8 @@
+## Frequency Counter
+This pattern uses objects or sets to collect values/frequencies of values which is usually o(n) time. This can avoid the need of nested loops or O(n^2) operations with arrays or strings.
+
+## Multiple Pointer
+This pattern creates pointers or value that correspond to an index or position and move towards the beginning, end, middle based on a certian condition. It's very efficient in solving problem with minimal space complexity as well. 
+
+## Sliding Window
+This pattern involves creating a window which can either be an array or number from one position to another. Depending on a certain condition, the window either increases or closes (and a new window is created). Its is very usefull in keeping track of a subset of data in an array or string e.t.c
---
+++ ProblemSolving/SolvingPatterns/slidingWindow.js
@@ -0,0 +1,40 @@
+// Write a function called maxSubArraySum which accepts
+// an array of integers and a number called n. The function
+// should calculate the maximum sum of n consecutive elements
+// in the array.
+
+// O(N^2)
+const maxSubArraySum = (arr, num) => {
+    if (num > arr.length) return null
+    let max = -Infinity
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        let temp = 0
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j]
+        }
+        if (temp > max) {
+            max = temp
+        }
+    }
+    return max
+}
+
+
+// Sliding Window Approach
+// O(N)
+const maxSubArraySum2 = (arr, num) => {
+    if (num > arr.length) return null
+    let maxSum = 0
+    let tempSum = 0
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+    tempSum = maxSum
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(tempSum, maxSum)
+    }
+    return maxSum
+}
+
+console.log(maxSubArraySum2([3, 4, 2, 4, 5, 6, 1, 1], 4))
