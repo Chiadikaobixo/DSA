5957ca576e34c2f5ed404c523b43724d43526645
chiadikaobixo <chiadikaobixo@gmail.com>
2022-06-19 16:09:54 -0500

    add-ons to quicksort

--- Algorithm/sortingAlgorithm/quickSort.js
+++ Algorithm/sortingAlgorithm/quickSort.js
@@ -1,5 +1,5 @@
-const quick = (array, startIndex = 0, endIndex=array.length + 1) => {
-    function swap(arr, i, j){
+const quick = (array, startIndex = 0, endIndex = array.length + 1) => {
+    function swap(arr, i, j) {
         var tem = arr[i]
         arr[i] = arr[j]
         arr[j] = tem
@@ -8,11 +8,21 @@ const quick = (array, startIndex = 0, endIndex=array.length + 1) => {
     let swapIndex = startIndex
     for (let i = startIndex + 1; i < array.length; i++) {
         if (pivot > array[i]) {
-           swapIndex++
-           swap(array, swapIndex, i)
+            swapIndex++
+            swap(array, swapIndex, i)
         }
     }
     swap(array, startIndex, swapIndex)
-    console.log(array)
     return swapIndex
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = quick(arr, left, right)
+        // left
+        quickSort(arr, left, pivotIndex - 1)
+        // right
+        quickSort(arr, pivotIndex + 1, right)
+    }
+    return arr
 }
