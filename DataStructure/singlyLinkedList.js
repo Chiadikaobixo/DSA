34aa01d678bdf2aa0ce689b74c301fabd5b10bbe
chiadikaobixo <chiadikaobixo@gmail.com>
2022-07-06 12:14:10 -0500

    update: singlylinkedlist

--- DataStructure/singlyLinkedList.js
+++ DataStructure/singlyLinkedList.js
@@ -23,4 +23,22 @@ class SinglyLinkedList{
         }
         this.length +=1
     }
+
+    pop(){
+        if(!this.head) return undefined
+        let current = this.head
+        let newtail = current
+        while(current.next){
+            newtail = current
+            current = current.next
+        }
+        this.tail = newtail
+        this.tail.next = null
+        this.length --
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return current
+    }
 }
